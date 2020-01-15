ff5d74e22d8c47bee9230823a2743504e4377f4c
Malibongwe Mduduzi Comfort MKHWANAZI <mmkhwana@c5r2s5.wethinkcode.co.za>
2020-01-15 17:58:26 +0200

    so far

---
+++ client/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <Login/>
+  </v-app>
+</template>
+
+<script>
+import Login from './components/Auth/Login'
+export default {
+  name: 'app',
+  components: {
+    Login
+  }
+}
+</script>
---
+++ client/src/components/Auth/Login.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-container>
+        <v-layout align-center justify-center>
+            <v-flex xs12 sm8 md4 >
+                <v-form>
+                    <v-card>
+                        <v-toolbar>
+                            <v-toolbar-tittle>
+                                Login Form
+                            </v-toolbar-tittle>
+                        </v-toolbar>
+                    </v-card>
+                </v-form>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+<script>
+export default {
+
+}
+</script>
