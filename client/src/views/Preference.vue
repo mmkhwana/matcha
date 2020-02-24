4a6f6d31ec41caf088dde985d1db72771d9b8fad
Khanyisa Mbukutshe <khanyisam@bbd.co.za>
2020-02-24 17:01:30 +0200

    wiseman

--- client/src/views/Preference.vue
+++ client/src/views/Preference.vue
@@ -36,7 +36,7 @@
         <v-overflow-btn
         class="my-2"
         v-model="location"
-        :items="language"
+        :items="results"
         label="Location preference"
         overflow
         target="#dropdown-example-2"
@@ -77,8 +77,20 @@ export default {
       item: 'Age preference',
       location: 'Location preference',
       dropdown_icon: [{ text: '18-21' }, { text: '22-25' }, { text: '26-29' }, { text: '30-33' }, { text: '34-37' }],
-      language: [{ text: 'English' }, { text: 'Xhosa' }, { text: 'Zulu' }, { text: 'Sotho' }, { text: 'Sepedi' }]
+      language: [{ text: 'English' }, { text: 'Xhosa' }, { text: 'Zulu' }, { text: 'Sotho' }, { text: 'Sepedi' }],
+      results: [],
+      city: ''
     }
+  },
+  mounted: {
+      results: fetch('https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyBlt9Sp7yGY_zrXiZx5NDQJS6lb17r4jco')
+        .then(result => result.json()).then(result => {
+          result.forEach(element => {
+            this.results.push(element.address.country)
+          })
+        }, error => {
+          alert(error.message)
+        })
   }
 }
 </script>
