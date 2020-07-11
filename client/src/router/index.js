bb131d39442455d62ed4a216479935487724325b
Mhlongo <Mandla.Mhlongo@vcontractor.co.za>
2020-07-11 23:04:48 +0200

    verify route

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,53 +3,66 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: () => import('../views/Home.vue')
-  },
-  {
-    path: '/welcome',
-    name: 'Login',
-    component: () => import('../views/Login.vue')
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: () => import('../views/About.vue')
-  },
-  {
-    path: '/Dashboard',
-    name: 'Dashboard',
-    component: () => import('../views/Dashboard.vue')
-  },
-  {
-    path: '/matches',
-    name: 'Matches',
-    component: () => import('../views/Matches.vue')
-  },
-  {
-    path: '/preference',
-    name: 'Preference',
-    component: () => import('../views/Preference.vue')
-  },
-  {
-    path: '/settings',
-    name: 'Settings',
-    component: () => import('../views/Settings.vue')
-  },
-  {
-    path: '/chat',
-    name: 'Chat',
-    component: () => import('../views/Chat.vue')
-  }
+const routes = [{
+        path: '/',
+        name: 'home',
+        component: () =>
+            import ('../views/Home.vue')
+    },
+    {
+        path: '/welcome',
+        name: 'Login',
+        component: () =>
+            import ('../views/Login.vue')
+    },
+    {
+        path: '/verify/:token/:email',
+        name: 'verify',
+        component: () =>
+            import ('../views/verify.vue')
+    },
+    {
+        path: '/about',
+        name: 'about',
+        component: () =>
+            import ('../views/About.vue')
+    },
+    {
+        path: '/Dashboard',
+        name: 'Dashboard',
+        component: () =>
+            import ('../views/Dashboard.vue')
+    },
+    {
+        path: '/matches',
+        name: 'Matches',
+        component: () =>
+            import ('../views/Matches.vue')
+    },
+    {
+        path: '/preference',
+        name: 'Preference',
+        component: () =>
+            import ('../views/Preference.vue')
+    },
+    {
+        path: '/settings',
+        name: 'Settings',
+        component: () =>
+            import ('../views/Settings.vue')
+    },
+    {
+        path: '/chat',
+        name: 'Chat',
+        component: () =>
+            import ('../views/Chat.vue')
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
